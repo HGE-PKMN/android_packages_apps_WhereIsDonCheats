7934ffef558c34867f0a5d9923ced9e1f03a2aa1
Myself5 <thchod@gmail.com>
2015-06-28 00:26:02 +0200

    Use Intents to cheat

--- app/src/main/java/de/myself5/whereisdoncheats/MainActivity.java
+++ app/src/main/java/de/myself5/whereisdoncheats/MainActivity.java
@@ -1,7 +1,6 @@
 package de.myself5.whereisdoncheats;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -13,26 +12,36 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
-    public static final String PREFS_NAME = "WIDPrefs";
     private EditText mETWonGames;
     private TextView mCurrentWonGames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mETWonGames = (EditText) findViewById(R.id.new_won_matches);
-        mCurrentWonGames = (TextView) findViewById(R.id.current_won_matches);
-        Button mUpdateWonGames = (Button) findViewById(R.id.update_won_matches);
 
-        mUpdateWonGames.setOnClickListener(
-                new View.OnClickListener() {
-                    public void onClick(View view) {
-                        update_won_games();
-                    }
-                });
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
 
-        updateTV();
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("WIDCheatCodeAnswer".equals(type)) {
+                updateTVPart2(intent); // Handle text being sent
+            }
+        } else {
+            setContentView(R.layout.activity_main);
+            mETWonGames = (EditText) findViewById(R.id.new_won_matches);
+            mCurrentWonGames = (TextView) findViewById(R.id.current_won_matches);
+            Button mUpdateWonGames = (Button) findViewById(R.id.update_won_matches);
+
+            mUpdateWonGames.setOnClickListener(
+                    new View.OnClickListener() {
+                        public void onClick(View view) {
+                            update_won_games();
+                        }
+                    });
+
+            updateTV();
+        }
     }
 
     public void update_won_games() {
@@ -40,20 +49,30 @@ public class MainActivity extends ActionBarActivity {
         if (!WonGamesS.equals("")) {
             int won_games = Integer.parseInt(WonGamesS);
             if (won_games > 0 && won_games <= 400) {
-                SharedPreferences settings = getSharedPreferences(PREFS_NAME, Context.MODE_WORLD_WRITEABLE);
-                SharedPreferences.Editor editor = settings.edit();
-                editor.putInt("won_games", won_games);
-                editor.apply();
+                sendIntent(WonGamesS, "WIDCheatCodeInput");
                 updateTV();
+            } else {
+                Toast.makeText(this, getString(R.string.invalid_won_matches), Toast.LENGTH_SHORT).show();
             }
         } else {
             Toast.makeText(this, getString(R.string.invalid_won_matches), Toast.LENGTH_SHORT).show();
         }
     }
 
+    private void sendIntent(String param, String type) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, param);
+        sendIntent.setType(type);
+        startActivity(sendIntent);
+    }
+
     private void updateTV() {
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, Context.MODE_WORLD_WRITEABLE);
-        int nFights = settings.getInt("won_games", 1);
-        mCurrentWonGames.setText(getString(R.string.current_won_matches) + " " + nFights);
+        sendIntent("", "WIDCheatCodeUpdate");
+    }
+
+    private void updateTVPart2(Intent intent) {
+        int n = Integer.parseInt(intent.getStringExtra(Intent.EXTRA_TEXT));
+        mCurrentWonGames.setText(getString(R.string.current_won_matches) + " " + n);
     }
 }
